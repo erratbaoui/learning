dcf1c44c2f7a2bb8cbf27b2a29a77db0f00315f4
Achraf Erratbaoui <erratbaouiachraf@gmail.com>
2025-10-07 23:49:58 +0100

    Add an error page

---
+++ src/main/java/CustomErrorController.java
@@ -0,0 +1,12 @@
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CustomErrorController implements ErrorController {
+
+    @RequestMapping("/error")
+    public String handleError() {
+        return "Oops! Something went wrong. This is a custom error message.";
+    }
+}
