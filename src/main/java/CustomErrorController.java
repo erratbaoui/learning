52ad98e20b66835260109fed0f90bde1206bcc4c
Achraf Erratbaoui <erratbaouiachraf@gmail.com>
2025-10-07 23:56:22 +0100

    Fix the  error page

--- src/main/java/CustomErrorController.java
+++ src/main/java/CustomErrorController.java
@@ -1,12 +1,25 @@
+import org.springframework.boot.web.error.ErrorAttributeOptions;
 import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.boot.web.servlet.error.ErrorAttributes;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+import java.util.Map;
 
 @RestController
 public class CustomErrorController implements ErrorController {
 
+    private final ErrorAttributes errorAttributes;
+
+    public CustomErrorController(ErrorAttributes errorAttributes) {
+        this.errorAttributes = errorAttributes;
+    }
+
     @RequestMapping("/error")
-    public String handleError() {
-        return "Oops! Something went wrong. This is a custom error message.";
+    public Map<String, Object> handleError(WebRequest webRequest) {
+        Map<String, Object> attributes = errorAttributes.getErrorAttributes(webRequest, ErrorAttributeOptions.defaults());
+        attributes.put("message", "Custom error message!");
+        return attributes;
     }
 }
